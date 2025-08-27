1c9c02babb67e34548a761f91d526d178aa978a4
xyliu3 <xyliu3@trip.com>
2025-08-27 19:31:07 +0800

    feat: rm mermaid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import rehypeMermaid from 'rehype-mermaid';
 import tailwindcss from "@tailwindcss/vite";
 import preact from "@astrojs/preact";
 import sitemap from "@astrojs/sitemap"
@@ -26,6 +25,5 @@ export default defineConfig({
       type: "shiki",
       excludeLangs: ["mermaid"]
     },
-    rehypePlugins: [[rehypeMermaid, { strategy: "img-svg", dark: true, colorScheme: "forest" }]]
   },
 });
